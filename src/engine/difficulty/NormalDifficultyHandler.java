23d5cd8dbcac2cf39cc9a4ee409c0e3262c38ae9
leesiyeon <sylee4880@naver.com>
2024-11-21 11:51:38 +0900

    refactor: NormalDifficultyHandler.adjustSettings 수정
    CSE2024SDP-166

--- src/engine/difficulty/NormalDifficultyHandler.java
+++ src/engine/difficulty/NormalDifficultyHandler.java
@@ -5,16 +5,18 @@ import engine.GameSettings;
 public class NormalDifficultyHandler implements DifficultyHandler {
     @Override
     public GameSettings adjustSettings(int formationWidth, int formationHeight, int baseSpeed, int shootingFrequency, int gameLevel) {
-        if (gameLevel % 2 == 0 && gameLevel < 5) {
+        boolean isEvenLevelUnderFive = (gameLevel % 2 == 0 && gameLevel < 5);
+        boolean isLevelAtLeastFive = gameLevel >= 5;
+
+        if (isEvenLevelUnderFive || isLevelAtLeastFive) {
+            int adjustBaseSpeed = isEvenLevelUnderFive ? 10 : 20;
+            int adjustShootingFrequency = isEvenLevelUnderFive ? 200 : 300;
+
             if (formationWidth == formationHeight && formationWidth < 14) formationWidth += 1;
             else if (formationHeight < 10) formationHeight += 1;
-            baseSpeed = Math.max(baseSpeed - 10, -150);
-            shootingFrequency = (shootingFrequency - 200 > 200) ? shootingFrequency - 200 : 100;
-        } else if (gameLevel >= 5) {
-            if (formationWidth == formationHeight && formationWidth < 14) formationWidth += 1;
-            else if (formationHeight < 10) formationHeight += 1;
-            baseSpeed = Math.max(baseSpeed - 20, -150);
-            shootingFrequency = (shootingFrequency - 300 > 300) ? shootingFrequency - 300 : 100;
+
+            baseSpeed = Math.max(baseSpeed - adjustBaseSpeed, -150);
+            shootingFrequency = (shootingFrequency - adjustShootingFrequency > adjustShootingFrequency) ? shootingFrequency - adjustShootingFrequency : 100;
         }
         return new GameSettings(formationWidth, formationHeight, baseSpeed, shootingFrequency);
     }
