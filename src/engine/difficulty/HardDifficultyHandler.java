b68eb8973424b4e5a7f68ec05d94ad4bea64d00c
leesiyeon <sylee4880@naver.com>
2024-11-21 11:52:17 +0900

    refactor: HardDifficultyHandler.adjustSettings 수정
    CSE2024SDP-166

--- src/engine/difficulty/HardDifficultyHandler.java
+++ src/engine/difficulty/HardDifficultyHandler.java
@@ -5,16 +5,20 @@ import engine.GameSettings;
 public class HardDifficultyHandler implements DifficultyHandler {
     @Override
     public GameSettings adjustSettings(int formationWidth, int formationHeight, int baseSpeed, int shootingFrequency, int gameLevel) {
-        if (gameLevel % 2 == 0 && gameLevel < 5) {
-            if (formationWidth == formationHeight && formationWidth < 14) formationWidth += 1;
-            else if (formationHeight < 10) formationHeight += 1;
+        boolean isEvenLevelUnderFive = (gameLevel % 2 == 0 && gameLevel < 5);
+        boolean isLevelAtLeastFive = gameLevel >= 5;
+
+        if (isEvenLevelUnderFive || isLevelAtLeastFive) {
+            int adjustFormationWidth = isEvenLevelUnderFive ? 1 : 2;
+            int adjustFormationHeight = isEvenLevelUnderFive ? 1 : 2;
+            int adjustShootingFrequency = isEvenLevelUnderFive ? 300 : 400;
+
+            if (formationWidth == formationHeight && formationWidth < 14) formationWidth += adjustFormationWidth;
+            else if (formationHeight < 10) formationHeight += adjustFormationHeight;
+
             baseSpeed = Math.max(baseSpeed - 20, -150);
-            shootingFrequency = (shootingFrequency - 300 > 300) ? shootingFrequency - 300 : 100;
-        } else if (gameLevel >= 5) {
-            if (formationWidth == formationHeight && formationWidth < 14) formationWidth += 2;
-            else if (formationHeight < 10) formationHeight += 2;
-            baseSpeed = Math.max(baseSpeed - 20, -150);
-            shootingFrequency = (shootingFrequency - 400 > 400) ? shootingFrequency - 400 : 100;
+            shootingFrequency = (shootingFrequency - adjustShootingFrequency > adjustShootingFrequency) ? shootingFrequency - adjustShootingFrequency : 100;
+
         }
         return new GameSettings(formationWidth, formationHeight, baseSpeed, shootingFrequency);
     }
