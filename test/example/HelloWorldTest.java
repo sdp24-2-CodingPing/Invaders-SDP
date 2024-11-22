45a1a6f94a2c9831f35f4d82fae857da68b90908
DayeonOh <dayeon.dev@gmail.com>
2024-11-22 19:46:19 +0900

    test: Add test code example
    CSE2024SDP-178

---
+++ test/example/HelloWorldTest.java
@@ -0,0 +1,14 @@
+package example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HelloWorldTest {
+
+    @Test
+    public void testHelloWorldMessage() {
+        String message = "Hello, World!";
+        assertEquals("Hello, World!", message, "The message should be 'Hello, World!'");
+    }
+}
