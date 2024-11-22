2c3151b217d0c610a0b935e79d8e1d0eb3341fce
ilranarli <shinilran@naver.com>
2024-11-22 20:44:34 +0900

    fix: merge 이후 발생한 버그 수정 CardSelectDrawManager 수정

--- src/engine/DrawManager.java
+++ src/engine/DrawManager.java
@@ -345,44 +345,6 @@ public class DrawManager {
 		drawCenteredBigString(screen, titleString, screen.getHeight() / 5);
 	}
 
-	public void drawCardSelectTitle(final Screen screen) {
-		String titleString = "Select The Card";
-		String instructionsString =
-				"select with arrows, confirm with space";
-
-		backBufferGraphics.setColor(Color.GRAY);
-		drawCenteredRegularString(screen, instructionsString,
-				screen.getHeight() / 5 * 2);
-
-		backBufferGraphics.setColor(Color.GREEN);
-		drawCenteredBigString(screen, titleString, screen.getHeight() / 5);
-	}
-
-	public void drawCard(final Screen screen, final Card card, final int width, final int height, final boolean isSelected) {
-		final int cardWidth = screen.getWidth() / 6;
-		final int cardHeight = screen.getHeight() / 6;
-		final String cardString = card.getCardName();
-
-		if (isSelected)	backBufferGraphics.setColor(Color.GREEN);
-		else 			backBufferGraphics.setColor(Color.DARK_GRAY);
-		backBufferGraphics.fillRect(width, height, cardWidth, cardHeight);
-
-		backBufferGraphics.setColor(Color.GRAY);
-		backBufferGraphics.setFont(fontBig);
-		backBufferGraphics.drawString(cardString, width + cardWidth / 2
-				- fontBigMetrics.stringWidth(cardString) / 2, height + cardHeight / 2);
-
-	}
-
-	// TODO: Card Design 추가 수정 필요
-	public void drawCardList(final Screen screen, final ArrayList<Card> cardList, final int selectedCardIndex) {
-		final int cardListSize = cardList.size();
-		for (int i=0; i < cardListSize; i++) {
-			drawCard(screen, cardList.get(i), screen.getWidth() / 3 * i + screen.getWidth() / 12, screen.getHeight() / 5 * 3, i==selectedCardIndex);
-		}
-	}
-
-
 	/**
 	 * Draws main menu.
 	 *
